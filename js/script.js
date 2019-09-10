377ccf764e35a01aa719ed1233a0b289f6811a3d
Vinicius Assis <believe.182@hotmail.com>
2019-09-10 20:43:17 -0300

    Add api in project

--- js/script.js
+++ js/script.js
@@ -6,8 +6,9 @@ import initModal from './modules/modal.js'
 import initTooltip from './modules/tooltip.js'
 import initDropDown from './modules/dropdown.js'
 import initMenuMobile from './modules/menu-mobile.js'
-import initAnimaNumeros from './modules/anima-numeros.js'
 import initFuncionamento from './modules/funcionamento.js'
+import initFetchAnimais from './modules/fetch-animais.js'
+import initFetchBitcoin from './modules/fetch-bitcoin.js'
 
 initScrollSuave()
 initAnimation()
@@ -17,5 +18,6 @@ initModal()
 initTooltip()
 initDropDown()
 initMenuMobile()
-initAnimaNumeros()
 initFuncionamento()
+initFetchAnimais()
+initFetchBitcoin()
