5c419d9cb179a419ac5a7f2736c77a365a2d6111
Vinicius Assis <believe.182@hotmail.com>
2019-09-10 09:43:23 -0300

    Add map's tooltip

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import initAnimation from './modules/scroll-animacao.js'
 import initAccordion from './modules/accordion.js'
 import initTabNav from './modules/tabnav.js'
 import initModal from './modules/modal.js'
+import initTooltip from './modules/tooltip.js'
 
 initScrollSuave()
 initAnimation()
 initAccordion()
 initTabNav()
 initModal()
+initTooltip()
