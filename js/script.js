f3955abdcfcc602ec727752b13f0e1ae328f0ece
Vinicius Assis <believe.182@hotmail.com>
2019-09-17 12:53:39 -0300

    Refatoring funcionamento using class

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 import Accordion from './modules/accordion.js'
 import DropDown from './modules/dropdown.js'
+import Funcionamento from './modules/funcionamento.js'
 import MenuMobile from './modules/menu-mobile.js'
 import Modal from './modules/modal.js'
 import ScrollAnima from './modules/scroll-anima.js'
@@ -8,7 +9,6 @@ import TabNav from './modules/tabnav.js'
 import Tooltip from './modules/tooltip.js'
 import fetchAnimais from './modules/fetch-animais.js'
 import fetchBitcoin from './modules/fetch-bitcoin.js'
-import initFuncionamento from './modules/funcionamento.js'
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"] ')
 scrollSuave.init()
@@ -34,7 +34,8 @@ dropDown.init()
 const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]')
 menuMobile.init()
 
-initFuncionamento()
+const funcionamento = new Funcionamento('[data-semana]', 'aberto')
+funcionamento.init()
 
 fetchAnimais('../../animaisapi.json', '.numeros-grid')
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco')
