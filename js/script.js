4e11480f1928a851c6ca0b7d875be2d9ec86face
Vinicius Assis <believe.182@hotmail.com>
2019-09-17 09:23:58 -0300

    Refatoring tooltip using class

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 import Accordion from './modules/accordion.js'
 import Modal from './modules/modal.js'
+import ScrollSuave from './modules/scroll-suave.js'
+import TabNav from './modules/tabnav.js'
+import Tooltip from './modules/tooltip.js'
 import initAnimation from './modules/scroll-animacao.js'
 import initDropDown from './modules/dropdown.js'
 import initFetchAnimais from './modules/fetch-animais.js'
 import initFetchBitcoin from './modules/fetch-bitcoin.js'
 import initFuncionamento from './modules/funcionamento.js'
 import initMenuMobile from './modules/menu-mobile.js'
-import initTooltip from './modules/tooltip.js'
-import ScrollSuave from './modules/scroll-suave.js'
-import TabNav from './modules/tabnav.js'
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"] ')
 scrollSuave.init()
@@ -22,9 +22,11 @@ tabNav.init()
 const modal = new Modal('[data-modal="container"]', '[data-modal="abrir"]', '[data-modal="fechar"]')
 modal.init()
 
+const toolTip = new Tooltip('[data-tooltip]')
+toolTip.init()
+
 
 initAnimation()
-initTooltip()
 initDropDown()
 initMenuMobile()
 initFuncionamento()
